4a767d7237f12e0332df4a36596e352c28175d86
sravyavulisetty <sravyavulisetty269@gmail.com>
2024-06-11 15:54:55 -0400

    error msg

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      return response.json();
+    })
     .then(data => {
       setCountries(data);
       setFilteredCountries(data);
