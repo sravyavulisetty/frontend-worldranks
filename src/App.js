cfd980fd5393b6c93c4c5ece89f283b2d0a570b1
sravyavulisetty <sravyavulisetty269@gmail.com>
2024-06-11 15:39:28 -0400

    router changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './Logo.svg';
 import image from './hero-image-wr.jpg';
 import './App.css';
 import HomePage from './pages/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CountryPage from './pages/CountryPage';
 
 function App() {
@@ -13,12 +13,12 @@ function App() {
         <img src={logo} className='logo' alt='logo'></img>
       </header>
       <main className='main'>
-          <BrowserRouter>
+          <Router>
           <Routes>
             <Route path='/' Component={HomePage}></Route>
             <Route path='/:country' Component={CountryPage}></Route>
           </Routes>
-          </BrowserRouter>
+          </Router>
       </main>
     </div>
   );
